f1d6ed417f5874603c8ff4636a6dcc6aca862fbf
Lalit Rana <lalit.rana@nagarro.com>
2021-07-24 15:28:34 +0530

    D3E3_W2 finished

---
+++ repos/Day3Exercise3_Way1/Day3Exercise3_Way1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day3Exercise3_Way1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There is no need for the application to be user interactive.");
+        }
+    }
+
+    class PriorityQueue<T> where T : IEquatable<T>
+    {
+        private IDictionary<int, IList<T>> elements;
+
+        public PriorityQueue()
+        {
+            elements = new SortedDictionary<int, IList<T>>();
+        }
+
+        public PriorityQueue(IDictionary<int, IList<T>> _elements):this()
+        {
+            this.elements = _elements;
+        }
+        public int count
+        {
+            get { return elements.Count; }
+        }
+
+        public bool Contains (T item)
+        {
+            return elements.Contains((KeyValuePair<int, IList<T>>)(object)item);
+        }
+
+        public T Dequeue()
+        {
+            var max = Peek();
+            elements.Remove(elements.Keys.Last());
+            return max;
+        }
+        public void Enqueue(int priority, T item)
+        {
+            try
+            {
+                elements.Add(priority, (IList<T>)(object)item);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("An element with Key = \"{0}\" already exists.", priority);
+            }
+            
+        }
+        public T Peek()
+        {
+            if (elements.Count == 0) throw new InvalidOperationException("Queue is empty.");
+            return (T)elements.Last().Value;
+        }
+
+        private int GetHighestPriority()
+        {
+            if(elements.Count == 0) throw new InvalidOperationException("Queue is empty.");
+            return elements.Last().Key;
+        }
+    }
+}
---
+++ repos/Day3Exercise3_Way2/Day3Exercise3_Way2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day3Exercise3_Way2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There is no need for the application to be user interactive.");
+        }
+    }
+
+    interface IPriority
+    {
+        int Priority
+        {
+            get;
+        }
+    }
+
+    class PriorityQueue2<T> where T : IEquatable<T>, IPriority
+    {
+        private int _num = 0;
+        private IDictionary<int, IList<T>> elements;
+
+        public int Priority
+        {
+            get => _num;
+        }
+
+        public PriorityQueue2()
+        {
+            elements = new SortedDictionary<int, IList<T>>();
+        }
+
+        public PriorityQueue2(IEnumerable<T> _elements) : this()
+        {
+            this.elements = (IDictionary<int, IList<T>>)_elements;
+        }
+        public int count
+        {
+            get { return elements.Count; }
+        }
+
+        public bool Contains(T item)
+        {
+            return elements.Contains((KeyValuePair<int, IList<T>>)(object)item);
+        }
+
+        public T Dequeue()
+        {
+            var max = Peek();
+            elements.Remove(elements.Keys.Last());
+            return max;
+        }
+        public void Enqueue(T item)
+        {
+            try
+            { 
+                elements.Add(++_num, (IList<T>)(object)item);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("An element with Key = \"{0}\" already exists.", _num);
+            }
+
+        }
+        public T Peek()
+        {
+            if (elements.Count == 0) throw new InvalidOperationException("Queue is empty.");
+            return (T)elements.Last().Value;
+        }
+
+        private int GetHighestPriority()
+        {
+            if (elements.Count == 0) throw new InvalidOperationException("Queue is empty.");
+            return elements.Last().Key;
+        }
+    }
+}
