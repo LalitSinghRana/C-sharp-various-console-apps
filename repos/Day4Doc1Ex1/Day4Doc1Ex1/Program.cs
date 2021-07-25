e45c9abd0ac4ba29e315f11d191ea39fdbfcbdce
Lalit Rana <lalit.rana@nagarro.com>
2021-07-26 00:38:42 +0530

    d4d1e2 finished

---
+++ repos/Day4Doc1Ex1/Day4Doc1Ex1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day4Doc1Ex1
+{
+    class Program
+    {
+        public delegate void Print(int value);
+        public delegate bool greaterThanFiveDelegate(int value);
+        public delegate bool find3kDelegate(int value);
+        public delegate bool find3k1Delegate(int value);
+        public delegate bool find3k2Delegate(int value);
+
+        public static bool greaterThanFive(int value)
+        {
+            if (value > 5) return true;
+            else return false;
+        }
+        public static bool find3k(int value)
+        {
+            if (value%3 == 0) return true;
+            else return false;
+        }
+        public static bool find3k2(int value)
+        {
+            if (value % 3 == 2) return true;
+            else return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<int> myList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //var temp = myList.Where(x => x==x);
+
+            //Odd numbers
+            var temp1 = myList.Where(x => x % 2 != 0);
+            foreach (var num in temp1) Console.Write(num + ", ");
+            Console.WriteLine("\n");
+
+            //Even numbers
+            var temp2 = myList.Where(x => {
+                return x % 2 == 0;
+                });
+            foreach (var num in temp2) Console.Write(num + ", ");
+            Console.WriteLine("\n");
+
+            //Prime numbers
+            Print prnt = delegate (int val) {
+                Console.Write(val + ", ");
+            };
+            foreach (var num in myList) if(num==2 || num==3 || num==5 || num==7)prnt(num);
+            Console.WriteLine("\n");
+
+            //Prime number lambda
+            Print prnt2 = val => Console.Write(val + ", ");
+            foreach (var num in myList) if (num == 2 || num == 3 || num == 5 || num == 7) prnt2(num);
+            Console.WriteLine("\n");
+
+            //Grater than 5
+            greaterThanFiveDelegate del = greaterThanFive;
+            //del = new greaterThanFiveDelegate(greaterThanFive);
+            foreach (var num in myList)
+            {
+                bool myBool = del(num);
+                if(myBool) Console.Write(num + ", ");
+            }
+            Console.WriteLine("\n");
+
+            //Grater than 5 another
+            var temp3 = myList.Where(x => {
+                greaterThanFiveDelegate delTemp = greaterThanFive;
+                return delTemp(x);
+            });
+            foreach (var num in temp3) Console.Write(num + ", ");
+            Console.WriteLine("\n");
+
+            //find 3k
+            find3kDelegate del2 = find3k;
+            var temp4 = myList.Where(x => del2(x));
+            foreach (var num in temp4) Console.Write(num + ", ");
+            Console.WriteLine("\n");
+
+            //find 3k+1
+            find3k1Delegate del3 = delegate (int val) {
+                if (val % 3 == 1) return true;
+                else return false;
+            };
+            var temp5 = myList.Where(x => del3(x));
+            foreach (var num in temp5) Console.Write(num + ", ");
+            Console.WriteLine("\n");
+
+            //find 3k+2
+            var temp6 = myList.Where(x => x%3==2);
+            foreach (var num in temp6) Console.Write(num + ", ");
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ repos/Day4Doc1Ex2/Day4Doc1Ex2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day4Doc1Ex2
+{
+    public delegate int EHsample(int a, int b);
+    class Program
+    {
+        public delegate void AddEventHandler(Dictionary<Product, int> d, Product p, int n, ref int tv);
+        static void Main(string[] args)
+        {
+            
+            var d = new Dictionary<Product, int>();
+            int tv = 0;
+            Random rdm = new Random();
+
+            Console.WriteLine("\ntv at start = {0}", tv);
+
+            for (int i=1; i<10; i++)
+            {
+                Product p = new Product(i, rdm.Next(100), false);
+                AddEventHandler del = new AddEventHandler(p.HandleSomethingAdded);
+                del(d, p, rdm.Next(10), ref tv);
+            }
+            Console.WriteLine("\ntv after adding = {0}", tv);
+
+
+            foreach (var temp in d)
+            {
+                if (temp.Key.id % 2 == 0)
+                {
+                    AddEventHandler del = new AddEventHandler(temp.Key.HandleSomethingRemoved);
+                    del(d, temp.Key, temp.Value, ref tv);
+                }
+            }
+            Console.WriteLine("\ntv after removing = {0}", tv);
+
+            foreach (var temp in d)
+            {
+                if (temp.Key.id % 2 != 0)
+                {
+                    AddEventHandler del = new AddEventHandler(temp.Key.HandleSomethingUpdated);
+                    del(d, temp.Key, rdm.Next(20), ref tv);
+                }
+            }
+            Console.WriteLine("\ntv after updating = {0}", tv);
+
+            foreach (var temp in d)
+            {
+                if (temp.Key.id % 3 == 0)
+                {
+                    AddEventHandler del = new AddEventHandler(temp.Key.HandleSomethingPrice);
+                    del(d, temp.Key, rdm.Next(50), ref tv);
+                }
+            }
+            Console.WriteLine("\ntv after price change = {0}", tv);
+
+        }
+
+        public class Product
+        {
+            public int id;
+            public int price;
+            public bool isDefective;
+
+            public Product(int a, int b, bool c)
+            {
+                id = a;
+                price = b;
+                isDefective = c;
+            }
+
+            public void HandleSomethingAdded(Dictionary<Product, int> d, Product p, int n, ref int tv)
+            {
+                d.Add(p, n);
+                tv += n * p.price;
+            }
+            public void HandleSomethingRemoved(Dictionary<Product, int> d, Product p, int n, ref int tv)
+            {
+                d.Remove(p);
+                tv -= n * p.price;
+            }
+            public void HandleSomethingUpdated(Dictionary<Product, int> d, Product p, int n, ref int tv)
+            {
+                int a = d[p];
+                d[p] = n;
+                tv += (n-a) * p.price;
+            }
+
+            public void HandleSomethingPrice(Dictionary<Product, int> d, Product p, int n, ref int tv)
+            {
+                Product x = new Product(p.id, n, p.isDefective);
+                int a = d[p];
+                tv -= a * p.price;
+                d.Remove(p);
+
+                AddEventHandler del = new AddEventHandler(p.HandleSomethingAdded);
+                del(d, x, a, ref tv);
+            }
+
+            public void HandleSomethingDefect(Dictionary<Product, int> d, Product p, int n, ref int tv)
+            {
+                if(n==0)
+                {
+                    AddEventHandler del = new AddEventHandler(p.HandleSomethingRemoved);
+                    del(d, p, d[p], ref tv);
+                } 
+            }
+        }
+    }
+
+    
+}
