78d253f50bfe637a7745859440a7a50833c22e68
Lalit Rana <lalit.rana@nagarro.com>
2021-07-26 15:35:35 +0530

    d4d1e1 updated

--- repos/Day4Doc1Ex1/Day4Doc1Ex1/Program.cs
+++ repos/Day4Doc1Ex1/Day4Doc1Ex1/Program.cs
@@ -6,36 +6,16 @@ namespace Day4Doc1Ex1
 {
     class Program
     {
-        public delegate void Print(int value);
-        public delegate bool greaterThanFiveDelegate(int value);
-        public delegate bool find3kDelegate(int value);
-        public delegate bool find3k1Delegate(int value);
-        public delegate bool find3k2Delegate(int value);
-
-        public static bool greaterThanFive(int value)
-        {
-            return value > 5;
-        }
-        public static bool find3k(int value)
-        {
-            if (value%3 == 0) return true;
-            else return false;
-        }
-        public static bool find3k2(int value)
-        {
-            if (value % 3 == 2) return true;
-            else return false;
-        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            List<int> myList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            //var temp = myList.Where(x => x==x);
+            List<int> myList = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
             //Odd numbers
             var temp1 = myList.Where(x => x % 2 != 0);
             foreach (var num in temp1) Console.Write(num + ", ");
             Console.WriteLine("\n");
+
 
             //Even numbers
             var temp2 = myList.Where(x => {
@@ -44,61 +24,90 @@ namespace Day4Doc1Ex1
             foreach (var num in temp2) Console.Write(num + ", ");
             Console.WriteLine("\n");
 
+
             //Prime numbers
-            Print prnt = delegate (int val) {
-                Console.Write(val + ", ");
-            };
-            foreach (var num in myList) if(num==2 || num==3 || num==5 || num==7)prnt(num);
+            Func<int, bool> isPrime = delegate (int number)
+                 {
+                     if (number <= 1) return false;
+                     if (number == 2) return true;
+
+                     var limit = Math.Ceiling(Math.Sqrt(number));
+
+                    for (int i = 2; i <= limit; ++i)
+                         if (number % i == 0)
+                             return false;
+                     return true;
+
+                 };
+            foreach (var num in myList) if(isPrime(num)) Console.Write(num+", ");
             Console.WriteLine("\n");
+
 
             //Prime number lambda
-            Print prnt2 = val => Console.Write(val + ", ");
-            foreach (var num in myList) if (num == 2 || num == 3 || num == 5 || num == 7) prnt2(num);
+            Func<int, bool> isPrime2 = number =>
+                 {
+                if (number <= 1) return false;
+                if (number == 2) return true;
+
+                var limit = Math.Ceiling(Math.Sqrt(number));
+
+                for (int i = 2; i <= limit; ++i)
+                    if (number % i == 0)
+                        return false;
+                return true;
+
+            };
+            foreach (var num in myList) if (isPrime2(num)) Console.Write(num + ", ");
             Console.WriteLine("\n");
+
 
             //Grater than 5
-            greaterThanFiveDelegate del = greaterThanFive;
-            //del = new greaterThanFiveDelegate(greaterThanFive);
-            foreach (var num in myList)
-            {
-                bool myBool = del(num);
-                if(myBool) Console.Write(num + ", ");
-            }
+            bool GreaterThan5(int x) => x > 5;
+            Func<int, bool> funcGreat = GreaterThan5;
+            var greaterThan5List = myList.Where(funcGreat);
+            foreach (var num in greaterThan5List) Console.Write(num + ", ");
             Console.WriteLine("\n");
 
-            //Grater than 5 another
-            var temp3 = myList.Where(x => {
-                greaterThanFiveDelegate delTemp = greaterThanFive;
-                return delTemp(x);
-            });
-            foreach (var num in temp3) Console.Write(num + ", ");
+
+            //Less than 5
+            bool LessThan5(int x) => x < 5;
+            Func<int, bool> funcLess = new Func<int, bool>(LessThan5);
+            var lessThan5List = myList.Where(funcLess);
+            foreach (var num in lessThan5List) Console.Write(num + ", ");
             Console.WriteLine("\n");
+
 
             //find 3k
-            find3kDelegate del2 = find3k;
-            var temp4 = myList.Where(x => del2(x));
-            foreach (var num in temp4) Console.Write(num + ", ");
+            Func<int, bool> func3k = new Func<int, bool>(x => x%3==0);
+            var find3kList = myList.Where(func3k);
+            foreach (var num in find3kList) Console.Write(num + ", ");
             Console.WriteLine("\n");
+
 
             //find 3k+1
-            find3k1Delegate del3 = delegate (int val) {
-                if (val % 3 == 1) return true;
-                else return false;
-            };
-            var temp5 = myList.Where(x => del3(x));
-            foreach (var num in temp5) Console.Write(num + ", ");
+            Func<int, bool> func3k1 = delegate (int x) { return x % 3 == 1; };
+            var find3k1List = myList.Where(func3k1);
+            foreach (var num in find3k1List) Console.Write(num + ", ");
             Console.WriteLine("\n");
 
+
             //find 3k+2
-            var temp6 = myList.Where(x => x%3==2);
-            foreach (var num in temp6) Console.Write(num + ", ");
+            var find3k2List = myList.Where(x => x % 3 == 2);
+            foreach (var num in find3k2List) Console.Write(num + ", ");
+            Console.WriteLine("\n");
+
+
+            //find Anything
+            var findAnyList = myList.Where(delegate (int x) { return x != 0; });
+            foreach (var num in findAnyList) Console.Write(num + ", ");
+            Console.WriteLine("\n");
+
+
+            //find Anything 2
+            bool FindAny(int x) => x != 5;
+            var findAnyList2 = myList.Where(FindAny);
+            foreach (var num in findAnyList2) Console.Write(num + ", ");
             Console.WriteLine("\n");
         }
     }
 }
-
-//bool LesserThan5(int num) => num < 5;
-//Func<int, bool> func2 = new Func<int, bool>(LesserThan5);
-//var temp4 = myList.Where(func2);
-//foreach (var num in temp4) Console.Write(num + ", ");
-//Console.WriteLine("\n");
